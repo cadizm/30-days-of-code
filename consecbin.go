fb8e2f2274d5391ca6aa9329e5ab8932494c8e2e
Michael Cadiz <mcadiz@truecar.com>
2017-02-12 12:12:39 -0800

    add consecbin

---
+++ consecbin.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanf("%d", &n)
+
+	curRun, maxRun := 0, 0
+
+	for n > 0 {
+		if n&1 == 1 {
+			curRun += 1
+			if curRun > maxRun {
+				maxRun = curRun
+			}
+		} else {
+			curRun = 0
+		}
+		n >>= 1
+	}
+
+	fmt.Println(maxRun)
+}
